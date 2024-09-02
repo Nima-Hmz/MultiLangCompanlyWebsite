c23184eb5684ef23bd4abd8df4c560b58210002f
nima-hmz <hmzmain11@gmail.com>
2024-09-02 15:06:03 +0330

    base.html implemented

--- paya_afraz/settings.py
+++ paya_afraz/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'paya_afraz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,6 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+
+
 
 
 
