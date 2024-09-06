74d1d95f3b65f927ecd2ab74957d0bb71ca7235b
nima-hmz <hmzmain11@gmail.com>
2024-09-06 14:41:51 +0330

    article_list with pagination and multilanguage is completed

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.views import View
 from .models import Article
 
@@ -7,9 +8,20 @@ from .models import Article
 
 class ArticleListView(View):
 	def get(self, request):
+		lang = request.GET.get('lang', 'fa')
+		valid_langs = ['en', 'fa']
+		if lang not in valid_langs:
+			lang = 'fa'
+
+		articles = Article.objects.filter(status=True)
+		paginator = Paginator(articles, 6)
+		page = request.GET.get('page')
+		article_list1 = paginator.get_page(page)
 		context = {
 
-			'article_temp':True
+			'article_temp':True,
+			'article_list1':article_list1,
+
 
 		}
 		return render(request, 'articles/article_list.html', context)
