a77d74e21c3f5e9c494a357673380696326d8040
Nima-Hmz <hmzmain11@gmail.com>
2024-09-09 15:56:03 +0330

    last changes before deploy

--- paya_afraz/urls.py
+++ paya_afraz/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('PayaRoot/', admin.site.urls),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('articles/', include('articles.urls')),
